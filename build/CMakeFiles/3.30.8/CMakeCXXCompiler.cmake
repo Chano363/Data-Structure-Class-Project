2c0ecfc3cf786c468cb4e59269120a09f866834f
Chano363 <3334309819@qq.com>
2025-05-28 20:53:23 +0800

    fix：修复CMakeLists中的硬编码错误

--- build/CMakeFiles/3.30.8/CMakeCXXCompiler.cmake
+++ build/CMakeFiles/3.30.8/CMakeCXXCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_CXX_COMPILER "D:/Code/MinGW/ucrt64/bin/c++.exe")
+set(CMAKE_CXX_COMPILER "D:/Code/MinGW/ucrt64/bin/g++.exe")
 set(CMAKE_CXX_COMPILER_ARG1 "")
 set(CMAKE_CXX_COMPILER_ID "GNU")
 set(CMAKE_CXX_COMPILER_VERSION "14.2.0")
--- build/CMakeFiles/parkingLotManagingSystem_static.dir/flags.make
+++ build/CMakeFiles/parkingLotManagingSystem_static.dir/flags.make
@@ -6,5 +6,5 @@ CXX_DEFINES =
 
 CXX_INCLUDES = @CMakeFiles/parkingLotManagingSystem_static.dir/includes_CXX.rsp
 
-CXX_FLAGS = -std=c++11
+CXX_FLAGS = -g -std=c++11
 
